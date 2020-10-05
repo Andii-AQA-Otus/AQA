4992a106ef777d18dc071f54da930872003199ab
Andii-AQA-Otus <72101233+Andii-AQA-Otus@users.noreply.github.com>
2020-10-05 16:16:13 +0300

    Add files via upload

--- JS_test/Second.js
+++ JS_test/Second.js
@@ -13,18 +13,21 @@ let person_2 = {
     height : 165,
 };
 
-for (ageTest in person_1) {
-    if (person_1[ageTest] == 30) {
-        console.log( 'It\'s Ok')
-    }
-    else {
-        console.log( 'It\'s not Ok')
-    }
-}
-console.log (person_1.name)
+// for (ageTest in person_1) {
+//     if (person_1[ageTest] == 30) {
+//         console.log( 'It\'s Ok')
+//     }
+//     else {
+//         console.log( 'It\'s not Ok')
+//     }
+// }
+// console.log (person_1.name)
 
 function forPerson_1(){
+    person_1.name = person_2.name
     person_1.age = person_2.age
+    person_1.weight = person_2.weight
+    person_1.height = person_2.height
 }
 forPerson_1()
 console.log (person_1)
