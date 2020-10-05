a2a17e25d6816f8bffd27ece33c729f1a3d6fafa
Andii-AQA-Otus <72101233+Andii-AQA-Otus@users.noreply.github.com>
2020-10-05 15:30:22 +0300

    Add files via upload

---
+++ JS_test/first.js
@@ -0,0 +1,31 @@
+/*
+// Вводим Число через консоль
+let number_1 = 6
+// Создаем Строку
+let number_2 = "10"
+
+//Преобразуем Число в Строку
+number_1 = String(number_1)
+console.log(number_1, " - ", typeof(number_1))
+//Преобразуем Строку в Число
+number_2 = Number(number_2)
+console.log(number_2, ' - ', typeof(number_2))
+
+// Конкaтенация
+let hello = 'Hello';
+let world = 'world';
+console.log(hello + " " + world)
+*/
+/*
+// Операции с числами
+let num_1 = 10;
+let num_2 = 5;
+num_3 = num_1 + num_2 // 15
+num_4 = num_1 - num_2 // 5
+num_5 = num_1 / num_2 // 2
+num_6 = num_3 % num_5 // 1
+num_7 = num_1 * num_2 // 50
+num_9 = num_2 ** 2 // 25
+num_10 = num_1 ^ 2
+console.log(num_10)
+*/
